68538df3d42af95cb203e92e94d45d532173aee1
David Youster <davidyouster@gmail.com>
2018-04-24 20:00:56 +0100

    Prompt before deleting a feed.

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -106,7 +106,9 @@ function createListNodeControlSection(bookmark) {
 }
 
 function onDeleteButtonClicked() {
-  browser.bookmarks.remove(this.dataset.bookmarkId).then(initSidebar);
+  if (confirm('Delete bookmark?')) {
+    browser.bookmarks.remove(this.dataset.bookmarkId).then(initSidebar);
+  }
 }
 
 function onFeedSelected(url, feedTitleContainer) {
