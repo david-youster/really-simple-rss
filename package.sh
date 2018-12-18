3e83f8e5e08124f2caac2d543a1b22251157a01b
David Youster <davidyouster@gmail.com>
2018-12-18 21:23:24 +0000

    Update package script to include options directory.

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-zip -r -FS simplerss-$1.zip background_scripts/ common/ content_scripts/ icons/ sidebar/ manifest.json  dialog/
+zip -r -FS simplerss-$1.zip background_scripts/ common/ content_scripts/ icons/ sidebar/ manifest.json  dialog/ options/
