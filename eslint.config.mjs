bf57f2667d6394b9a4c30eeb1a6b9f892f9903ea
David Youster <davidyouster@gmail.com>
2025-04-26 23:25:17 +0100

    Update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended"),
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            browser: false,
+        },
+
+        ecmaVersion: 2017,
+        sourceType: "script",
+    },
+
+    rules: {
+        "max-len": ["error"],
+        indent: ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "single"],
+        semi: ["error", "always"],
+    },
+}]);
